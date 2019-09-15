9fcc3a3475a6b43818cb8183dd64367a8b1e2151
Daniel Peixoto Pinto da Silva <peixoto.daniel@hotmail.com>
2019-09-15 03:22:16 -0300

    Correções

--- TypeScript/scripts/poo.js
+++ TypeScript/scripts/poo.js
@@ -15,3 +15,28 @@ let programador = new Funcionario('Bruce', 'Wayne', 'Programador');
 console.log(programador.cumprimentar('Daniel'));
 programador.salario(99999);
 programador.salario('99999');
+class Planeta {
+    constructor(nome, circunferencia, Luas = []) {
+        this.addLua = (Lua) => {
+            this.Luas.push(Lua);
+        };
+        this.nome = nome;
+        this.circunferencia = circunferencia;
+        this.Luas = Luas;
+    }
+    ;
+}
+class Terra extends Planeta {
+    constructor() {
+        super(...arguments);
+        this.rotationMetersPerSecond = 460;
+        this.metersInMile = 1609.34;
+        this.secondsInMinute = 60;
+        this.minutesInHour = 60;
+        this.getRotationSpeedMph = () => {
+            return (this.rotationMetersPerSecond * this.secondsInMinute * this.minutesInHour) / this.metersInMile;
+        };
+    }
+}
+const terra = new Terra('Terra', 24901, [{ nome: 'Lua', circunferencia: 6786 }]);
+console.log(`Oi! Meu nome é ${terra.nome} e eu estou girando a ${terra.getRotationSpeedMph().toFixed(2)} mph!`);
--- TypeScript/scripts/poo.ts
+++ TypeScript/scripts/poo.ts
@@ -1,30 +1,69 @@
-interface IPessoa{
-    nome:string;
-    sobrenome:string;
-    cumprimentar:(nome:string)=>string;
+interface IPessoa {
+    nome: string;
+    sobrenome: string;
+    cumprimentar: (nome: string) => string;
 }
 
-class Funcionario implements IPessoa{
-    nome:string;
-    sobrenome:string;
-    funcao:string;
-    constructor(nome:string , sobrenome:string,funcao:string){
+class Funcionario implements IPessoa {
+    nome: string;
+    sobrenome: string;
+    funcao: string;
+    constructor(nome: string, sobrenome: string, funcao: string) {
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.funcao = funcao;
     }
-    cumprimentar = (nomePessoa:string):string=>{
+    cumprimentar = (nomePessoa: string): string => {
         return `Olá ${nomePessoa}, meu nome é ${this.nome} e eu sou ${this.funcao}`;
     }
-    salario = (valor:number | string ):void=>{
+    salario = (valor: number | string): void => {
         console.log(valor.valueOf());
     }
 }
 
-let programador = new Funcionario('Bruce','Wayne','Programador');
+let programador = new Funcionario('Bruce', 'Wayne', 'Programador');
 
 console.log(programador.cumprimentar('Daniel'));
 
 programador.salario(99999);
 programador.salario('99999');
+//////////////////////////////////////////////////////////////////
 
+interface Lua {
+    nome: string;
+    circunferencia: number;
+}
+
+abstract class Planeta {
+    public nome: string;
+    public circunferencia: number;
+    public Luas: Lua[];
+
+    constructor(nome: string, circunferencia: number, Luas: Lua[] = []) {
+        this.nome = nome;
+        this.circunferencia = circunferencia;
+        this.Luas = Luas;
+    };
+
+    addLua = (Lua: Lua) => {
+        this.Luas.push(Lua);
+    };
+
+    abstract getRotationSpeedMph: () => number;
+}
+
+class Terra extends Planeta {
+    private rotationMetersPerSecond = 460;
+    private metersInMile = 1609.34;
+    private secondsInMinute = 60;
+    private minutesInHour = 60;
+
+    getRotationSpeedMph = () => {
+        return (this.rotationMetersPerSecond * this.secondsInMinute * this.minutesInHour) / this.metersInMile;
+    }
+}
+
+
+const terra = new Terra('Terra', 24901, [{ nome: 'Lua', circunferencia: 6786 } as Lua]);
+
+console.log(`Oi! Meu nome é ${terra.nome} e eu estou girando a ${terra.getRotationSpeedMph().toFixed(2)} mph!`);
